b8998bf4c5a4f4363f1a6d8ab7fad8c1b1d0947d
Jefferson David Condiza Quintero <jefferdcondiza@gmail.com>
2025-10-22 19:41:59 -0500

    chore: add standard as a dev dependency for code linting

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -1,29 +1,28 @@
 import { useState } from "react";
 import confetti from "canvas-confetti";
-import Square  from "./components/Square";
+import Square from "./components/Square";
 import { TURNS } from "./constans";
 import { checkWinnerFrom, checkEndGame } from "./logic/board";
 import { saveGameToStorage, resetGameStorage } from "./logic/storage";
 import { WinnerModal } from "./components/WinnerModal";
-import './App.css'
-
+import "./App.css";
 
 function App() {
-// inicializar el tablero y almacenarlo en localStorage
-  const [board , setBoard] = useState(()=>{
-    const boardFromStorage = window.localStorage.getItem('board');
+  // inicializar el tablero y almacenarlo en localStorage
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
     if (boardFromStorage) return JSON.parse(boardFromStorage);
     return Array(9).fill(null);
   });
 
   // cambiar el turno y almacenar en localStorage
-  const [turn , setTurn] = useState(()=>{
-    const turnFromStorage = window.localStorage.getItem('turn');
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
     return turnFromStorage ?? TURNS.X;
   });
 
   // null significa que no hay ganador
-  const [winner , setWinner] = useState(null);
+  const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
@@ -31,7 +30,7 @@ function App() {
     setWinner(null);
 
     resetGameStorage();
-  }
+  };
 
   // logica para actualizar el tablero
   const updateBoard = (index) => {
@@ -45,7 +44,7 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
     // guardar el estado del juego en el localStorage
-    saveGameToStorage({board: newBoard, turn: newTurn});
+    saveGameToStorage({ board: newBoard, turn: newTurn });
     // revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
@@ -54,39 +53,30 @@ function App() {
     } else if (checkEndGame(newBoard)) {
       setWinner(false); // empate
     }
-  }
+  };
 
   return (
-    <main className='board'>
+    <main className="board">
       <h1 translate="no">Tic tac toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
-      <section className='game'>
-        {
-          board.map((square, index) => {
-            return (
-              <Square
-                key={index}
-                index={index}
-                updateBoard={updateBoard}
-              >
-                {square}
-              </Square>
-            )
-          })
-        }
+      <section className="game">
+        {board.map((square, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
+            </Square>
+          );
+        })}
       </section>
 
-      <section className='turn'>
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-        </Square>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
       <WinnerModal resetGame={resetGame} winner={winner} />
-    </main>  )
+    </main>
+  );
 }
 
-export default App
+export default App;
